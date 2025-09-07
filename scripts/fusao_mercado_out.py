b215fea28dba9a037b26bf1f27e71650aef51d15
Vagner Michell <vagner.michell@gmail.com>
2025-09-07 15:44:16 -0300

    fix: adicionado classes

--- scripts/fusao_mercado_out.py
+++ scripts/fusao_mercado_out.py
@@ -1,123 +1,35 @@
 # %%
-import json
-import csv
 from processamento_dados import Dados
-
-#%%
-
-# def leitura_json(path_json):
-#     with open(path_json, 'r') as file:
-#         dados_json = json.load(file)
-
-#     return dados_json
-
-# def leitura_csv(path_csv):
-#     dados_csv = []
-#     with open (path_csv, 'r') as file:
-#         spamreader = csv.DictReader(file, delimiter=',')
-        
-#         for linha in spamreader:
-#             dados_csv.append(linha)
-
-#     return dados_csv
-
-# def leitura_dados(path, tipo_arquivo):
-
-#     if tipo_arquivo == 'csv':
-#         dados = leitura_csv(path)
-        
-
-#     elif tipo_arquivo == 'json':
-#         dados = leitura_json(path)
-
-#     return dados
-
-def identificar_colunas(dados):
-    return list(dados[0].keys())
-
-def renomear_colunas(dados, key_mapping):
-    new_dados_csv = [{key_mapping.get(old_key): value for old_key, value in old_dict.items()} for old_dict in dados ]
-    return new_dados_csv
-
-def tamanho_arquivo(dados):
-    return len(dados)
-
-def fusao(dadosA, dadosB):
-    combined_list = []
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list
-
-def transformando_dados_tabela(dados, nomes_colunas):
-    
-    dados_combinados_tabela = [nomes_colunas]
-
-    for row in dados:
-        linha = [] 
-        for coluna in nomes_colunas:
-            linha.append(row.get(coluna, 'Indisponivel'))
-        dados_combinados_tabela.append(linha)
-
-    return dados_combinados_tabela
-
-def salvando_dados(dados, caminho):
-    with open(caminho, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerows(dados)
 #%%
 
 path_json = '../data_raw/dados_empresaA.json'
 path_csv = '../data_raw/dados_empresaB.csv'
 
 dados_empresaA = Dados(path_json, 'json')
-print(dados_empresaA.dados)
+print(f'Os dados da empresaA: {dados_empresaA.nome_colunas} com {dados_empresaA.qtd_linhas} linhas.')
 
 dados_empresaB = Dados(path_csv, 'csv')
-print(dados_empresaB.dados)
+print(f'Os dados da empresaB: {dados_empresaB.nome_colunas} com {dados_empresaB.qtd_linhas} linhas.')
 
-# ## Iniciando leitura
+#Transformar
 
-# dados_json = leitura_dados(path_json, 'json')
-# dados_csv = leitura_dados(path_csv, 'csv')
-# tamanho_dados_json = tamanho_arquivo(dados_json)
-# tamanho_dados_csv = tamanho_arquivo(dados_csv)
+key_mapping = {'Nome do Item': 'Nome do Produto', 
+               'Classificação do Produto': 'Categoria do Produto',
+               'Valor em Reais (R$)': 'Preço do Produto (R$)',
+               'Quantidade em Estoque': 'Quantidade em Estoque',
+               'Nome da Loja': 'Filial',
+               'Data da Venda': 'Data da Venda'
+               }
 
-# nome_colunas_json = identificar_colunas(dados_json)
-# nome_colunas_csv = identificar_colunas(dados_csv)
+dados_empresaB.renomear_colunas(key_mapping)
 
-# # %%
+print(f'Novos nomes de coluna da empresa B {dados_empresaB.nome_colunas}')
 
-# #Transformação dados
+dados_fusao = Dados.fusao(dados_empresaA, dados_empresaB)
+print(f'A junção dos dados trouxe as colunas: {dados_fusao.nome_colunas} e {dados_fusao.qtd_linhas} linhas retornadas')
 
-# key_mapping = {'Nome do Item': 'Nome do Produto', 
-#                'Classificação do Produto': 'Categoria do Produto',
-#                'Valor em Reais (R$)': 'Preço do Produto (R$)',
-#                'Quantidade em Estoque': 'Quantidade em Estoque',
-#                'Nome da Loja': 'Filial',
-#                'Data da Venda': 'Data da Venda'
-#                }
+## Load
 
-# dados_csv = renomear_colunas(dados_csv, key_mapping)
-# nome_colunas_csv = identificar_colunas(dados_csv)
-# print(f"Tamanho arquivos json: {tamanho_dados_json}")
-# print(f"Tamanho arquivos csv: {tamanho_dados_csv}")
-
-# dados_fusao = fusao(dados_csv, dados_json)
-# nome_colunas_fusao = identificar_colunas(dados_fusao)
-# tamanho_dados_fusao = tamanho_arquivo(dados_fusao)
-
-# print(f"Tamanho arquivos fusao: {tamanho_dados_fusao}")
-
-# # %%
-
-# #Salvando dados
-
-# dados_fusao_tabela = transformando_dados_tabela(dados_fusao, nome_colunas_fusao)
-
-# path_dados_combinados = '../data_processed/dados_combinados.csv'
-
-# salvando_dados(dados_fusao_tabela, path_dados_combinados)
-
-# print(f'Arquivo salvo em: {path_dados_combinados}')
-
-# %%
+caminho_destino = '../data_processed/dados_combinados.csv'
+dados_fusao.salvando_dados(caminho_destino)
+print(f'Arquivo salvo em: {caminho_destino}')
--- scripts/processamento_dados.py
+++ scripts/processamento_dados.py
@@ -7,6 +7,8 @@ class Dados:
         self.path = path 
         self.tipo_dados = tipo_dados
         self.dados = self.leitura_dados()
+        self.nome_colunas = self.identificar_colunas()
+        self.qtd_linhas = self.tamanho_arquivo()
 
     def leitura_json(self):
         with open(self.path, 'r') as file:
@@ -32,4 +34,46 @@ class Dados:
         elif self.tipo_dados == 'json':
             dados = self.leitura_json()
 
+        elif self.tipo_dados == 'list':
+            dados = self.path
+            self.path = 'lista em memoria'
+
         return dados
+    
+    def identificar_colunas(self):
+        return list(self.dados[-1].keys())
+    
+    def renomear_colunas(self, key_mapping):
+        new_dados_csv = [{key_mapping.get(old_key): value for old_key, value in old_dict.items()} for old_dict in self.dados ]
+        
+        self.dados = new_dados_csv
+        self.nome_colunas = self.identificar_colunas()
+        
+    def tamanho_arquivo(self):
+        return len(self.dados)
+    
+    def fusao(dadosA, dadosB):
+        combined_list = []
+        combined_list.extend(dadosA.dados)
+        combined_list.extend(dadosB.dados)
+        return Dados(combined_list, 'list')
+    
+    def transformando_dados_tabela(self):
+    
+        dados_combinados_tabela = [self.nome_colunas]
+
+        for row in self.dados:
+            linha = [] 
+            for coluna in self.nome_colunas:
+                linha.append(row.get(coluna, 'Indisponivel'))
+            dados_combinados_tabela.append(linha)
+
+        return dados_combinados_tabela
+    
+    def salvando_dados(self, caminho):
+
+        dados_combinados_tabela = self.transformando_dados_tabela()
+
+        with open(caminho, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(dados_combinados_tabela)
